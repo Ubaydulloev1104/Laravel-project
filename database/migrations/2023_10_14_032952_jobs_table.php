55e15a5221cb8f766efbc1222cb4e4ba5078256c
Azamjon <116807178+Ubaydulloev1104@users.noreply.github.com>
2023-10-14 08:47:51 +0500

    Add migration

---
+++ database/migrations/2023_10_14_032952_jobs_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class JobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+         Schema::create('jobs', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->double('salary');
+            $table->integer('company_id');
+            $table->integer('type_id');
+            $table->integer('location_id');
+            $table->text('description');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('jobs');
+    }
+    
+}
